46b640398ba141cc1bb7ccc10deffa08d555b63c
KostianDev <verbytskyi.kostiantyn@lll.kpi.ua>
2025-04-03 22:25:53 +0300

    Remove list methods, create custom linked list constructor

--- src/list.go
+++ src/list.go
@@ -1,93 +1,17 @@
 package lists
 
-import "errors"
-
-type List struct {
-	elements []rune
+type Node struct {
+	value rune
+	next  *Node
+	previous *Node
 }
 
-func NewList() *List {
-	return &List{
-		elements: make([]rune, 0),
-	}
+type LinkedList struct {
+	head *Node
+	tail *Node
+	length int
 }
 
-func (l *List) Length() int {
-	return len(l.elements)
-}
-
-func (l *List) Append(element rune) {
-	l.elements = append(l.elements, element)
-}
-
-func (l *List) Insert(element rune, index int) error {
-	if index < 0 || index > len(l.elements) {
-		return errors.New("index out of bounds")
-	}
-	l.elements = append(l.elements[:index], append([]rune{element}, l.elements[index:]...)...)
-	return nil
-}
-
-func (l *List) Delete(index int) (rune, error) {
-	if index < 0 || index >= len(l.elements) {
-		return 0, errors.New("index out of bounds")
-	}
-	element := l.elements[index]
-	l.elements = append(l.elements[:index], l.elements[index+1:]...)
-	return element, nil
-}
-
-func (l *List) DeleteAll(element rune) {
-	for i := 0; i < len(l.elements); i++ {
-		if l.elements[i] == element {
-			l.elements = append(l.elements[:i], l.elements[i+1:]...)
-			i--
-		}
-	}
-}
-
-func (l *List) Get(index int) (rune, error) {
-	if index < 0 || index >= len(l.elements) {
-		return 0, errors.New("index out of bounds")
-	}
-	return l.elements[index], nil
-}
-
-func (l *List) Clone() ListInterface {
-	clone := NewList()
-	clone.elements = make([]rune, l.Length())
-	copy(clone.elements, l.elements)
-	return clone
-}
-
-func (l *List) Reverse() {
-	for i, j := 0, len(l.elements)-1; i < j; i, j = i+1, j-1 {
-		l.elements[i], l.elements[j] = l.elements[j], l.elements[i]
-	}
-}
-
-func (l *List) FindFirst(element rune) int {
-	for i, e := range l.elements {
-		if e == element {
-			return i
-		}
-	}
-	return -1
-}
-
-func (l *List) FindLast(element rune) int {
-	for i := len(l.elements) - 1; i >= 0; i-- {
-		if l.elements[i] == element {
-			return i
-		}
-	}
-	return -1
-}
-
-func (l *List) Clear() {
-	l.elements = make([]rune, 0)
-}
-
-func (l *List) Extend(other ListInterface) {
-	l.elements = append(l.elements, other.(*List).elements...)
+func NewList() *LinkedList {
+	return &LinkedList{nil, nil, 0}
 }
