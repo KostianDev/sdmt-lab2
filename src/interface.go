4dd6b97a32adae2748d0447413f8b7cb5e47717b
KostianDev <verbytskyi.kostiantyn@lll.kpi.ua>
2025-04-03 18:29:58 +0300

    Create main list interface

---
+++ src/interface.go
@@ -0,0 +1,16 @@
+package main
+
+type ListInterface interface {
+	Length() int
+	Append(element rune)
+	Insert(element rune, index int) error
+	Delete(index int) (rune, error)
+	DeleteAll(element rune)
+	Get(index int) (rune, error)
+	Clone() ListInterface
+	Reverse()
+	FindFirst(element rune) int
+	FindLast(element rune) int
+	Clear()
+	Extend(other ListInterface)
+}
