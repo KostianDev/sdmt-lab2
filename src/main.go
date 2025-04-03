e5ea488498b77fbc94271331a78c5a7ade72f160
KostianDev <verbytskyi.kostiantyn@lll.kpi.ua>
2025-04-03 19:26:41 +0300

    Translate console output to English for consistency

--- src/main.go
+++ src/main.go
@@ -13,10 +13,10 @@ func demoList(list ListInterface) {
 	list.Append('a')
 	list.Append('b')
 	list.Append('c')
-	fmt.Println("Після Append('a', 'b', 'c'):", listToString(list))
+	fmt.Println("After Append('a', 'b', 'c'):", listToString(list))
 	
 	list.Insert('x', 1)
-	fmt.Println("Після Insert('x', 1):", listToString(list))
+	fmt.Println("After Insert('x', 1):", listToString(list))
 	
 	val, _ := list.Get(2)
 	fmt.Println("Get(2):", string(val))
@@ -26,32 +26,32 @@ func demoList(list ListInterface) {
 	fmt.Println("FindFirst('b'):", list.FindFirst('b'))
 	
 	list.Append('b')
-	fmt.Println("Після Append('b'):", listToString(list))
+	fmt.Println("After Append('b'):", listToString(list))
 	fmt.Println("FindLast('b'):", list.FindLast('b'))
 	
 	deleted, _ := list.Delete(0)
 	fmt.Println("Delete(0):", string(deleted), "->", listToString(list))
 	
 	list.DeleteAll('b')
-	fmt.Println("Після DeleteAll('b'):", listToString(list))
+	fmt.Println("After DeleteAll('b'):", listToString(list))
 	
 	clone := list.Clone()
 	clone.Append('y')
-	fmt.Println("Оригінал:", listToString(list))
-	fmt.Println("Клон з доданим 'y':", listToString(clone))
+	fmt.Println("Original:", listToString(list))
+	fmt.Println("Clone with added 'y':", listToString(clone))
 	
 	list.Reverse()
-	fmt.Println("Після Reverse:", listToString(list))
+	fmt.Println("After Reverse:", listToString(list))
 	
 	other := NewList()
 	other.Append('1')
 	other.Append('2')
 	list.Extend(other)
-	fmt.Println("Після Extend з [1, 2]:", listToString(list))
-	fmt.Println("Другий список після операції:", listToString(other))
+	fmt.Println("After Extend with [1, 2]:", listToString(list))
+	fmt.Println("Second list after operation:", listToString(other))
 	
 	list.Clear()
-	fmt.Println("Після Clear:", listToString(list))
+	fmt.Println("After Clear:", listToString(list))
 }
 
 func listToString(list ListInterface) string {
